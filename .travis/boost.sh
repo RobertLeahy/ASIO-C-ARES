05e12dfc8c5e45725fd57464e97966522b84c1e9
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2017-08-27 03:23:35 -0400

    Travis CI: Boost 1.65.0

--- .travis/boost.sh
+++ .travis/boost.sh
@@ -2,7 +2,7 @@
 set -x
 mkdir boost
 pushd boost
-if [ ${2} = "64" ]; then BOOST_URL="https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.gz"
+if [ ${2} = "64" ] || [ ${2} = "65" ]; then BOOST_URL="https://dl.bintray.com/boostorg/release/1.${2}.0/source/boost_1_${2}_0.tar.gz"
 else BOOST_URL="https://downloads.sourceforge.net/project/boost/boost/1.${2}.0/boost_1_${2}_0.tar.gz"
 fi
 wget -nc ${BOOST_URL}
--- .travis/cmake.sh
+++ .travis/cmake.sh
@@ -2,8 +2,10 @@
 set -x
 mkdir cmake
 pushd cmake
-wget -nc https://cmake.org/files/v3.8/cmake-3.8.1-Linux-x86_64.sh
-chmod 755 cmake-3.8.1-Linux-x86_64.sh
-sed -i -r "s/^interactive=TRUE$/interactive=FALSE/g" cmake-3.8.1-Linux-x86_64.sh
-./cmake-3.8.1-Linux-x86_64.sh
+wget -nc https://cmake.org/files/v3.9/cmake-3.9.1-Linux-x86_64.sh
+chmod 755 cmake-3.9.1-Linux-x86_64.sh
+sed -i -r "s/^interactive=TRUE$/interactive=FALSE/g" cmake-3.9.1-Linux-x86_64.sh
+./cmake-3.9.1-Linux-x86_64.sh
+wget -nc https://raw.githubusercontent.com/Kitware/CMake/e710d6953dae27f73452095df6d7b2d7ec698fd1/Modules/FindBoost.cmake
+cp FindBoost.cmake ./share/cmake-3.9/Modules/
 popd
